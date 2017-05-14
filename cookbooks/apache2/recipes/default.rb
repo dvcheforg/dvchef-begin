acd0de71a52689568867ef53e304589edd2ea098
dvcheforg <dvcheforg@gmail.com>
2017-05-14 20:13:40 +0000

    update - using hostname and platform_family

--- cookbooks/apache2/recipes/default.rb
+++ cookbooks/apache2/recipes/default.rb
@@ -4,8 +4,15 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 #
+
+if node('platform_family') == "rhel"
+    package_toget = "httpd"
+elsif node('platform_family') == "debian"
+    package_toget = "apache2"
+end
+
 package 'apache2' do
-    package_name 'httpd'
+    package_name package_toget
     action :install
 end
 
